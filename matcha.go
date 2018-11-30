ddb5cba4cf5b86b16df080628cff64f33647a191
Gladwin MOHLAMONYANE <gmohlamo@e5r11p3.wethinkcode.co.za>
2018-11-30 14:16:25 +0200

    got the server to return a json file when requesteed to do something about it

--- matcha.go
+++ matcha.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
 	"os"
 	"strings"
+
+	"github.com/blavkboy/matcha/models"
+	"github.com/gorilla/mux"
 )
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
@@ -22,6 +26,17 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+//handle Users will handle requests to get the users from the browser.
+func handleUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	d := &models.User{ID: "skjhgdjahds", Username: "The dark one", Fname: "El Pharoah", Email: "akjhdskjahsdkjh@sponges.com", Password: "akjsdghhjakhdksjadklashdklad"}
+	err := json.NewEncoder(w).Encode(d)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
 //this data struct will hold all the dependencies
 //needed to provide our web service
 type data struct {
@@ -30,6 +45,10 @@ type data struct {
 
 //In main we will handle all requests to the server
 func main() {
-	http.HandleFunc("/", handleRoot)
-	http.ListenAndServe(":8080", nil)
+	r := mux.NewRouter()
+	fs := http.FileServer(http.Dir("assets/"))
+	http.Handle("/assets/", http.StripPrefix("/static/", fs))
+	r.HandleFunc("/", handleRoot)
+	r.HandleFunc("/users/{id}", handleUser)
+	http.ListenAndServe(":8080", r)
 }
