f5597add9715d966c82b683dfb6e6845cf8cf14d
Gladwin Tshepo Mohlamonyane <g.mohlamonyane@gmail.com>
2018-11-30 23:24:36 +0200

    learning how to use the parsing engine

--- matcha.go
+++ matcha.go
@@ -26,14 +26,56 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+//array we will keep users in
+var users []models.User
+
+//sample user to add to the array
+var d = &models.User{
+	ID:       "1",
+	Username: "The dark one",
+	Fname:    "El Pharoah",
+	Lname:    "",
+	Email:    "akjhdskjahsdkjh@sponges.com",
+	Password: "akjsdghhjakhdksjadklashdklad",
+	Bio: &models.Bio{
+		Caddress: nil,
+		Oaddress: &models.Address{
+			Street1:  "352",
+			Street2:  "Du Toit Street",
+			Suburb:   "Wierda Park",
+			City:     "Pretoria",
+			Province: "Gauteng",
+		},
+		Sexuality: &models.Sexuality{
+			Sex:         models.Male,
+			Orientation: models.Hetero,
+			Looking:     models.Fun,
+			Preferences: nil,
+		},
+		Hobbies:   nil,
+		Interests: nil,
+	},
+}
+
 //handle Users will handle requests to get the users from the browser.
 func handleUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	d := &models.User{ID: "skjhgdjahds", Username: "The dark one", Fname: "El Pharoah", Email: "akjhdskjahsdkjh@sponges.com", Password: "akjsdghhjakhdksjadklashdklad"}
-	err := json.NewEncoder(w).Encode(d)
-	if err != nil {
-		fmt.Println(err)
-		return
+	//vars := mux.Vars(r)
+	if r.Method == "GET" {
+		tmpl, err := template.ParseFiles("views/root.html")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		tmpl.Execute(w, data{
+			PageTitle: "Test Page",
+			Data:      users,
+		})
+	} else if r.Method == "POST" {
+		w.Header().Set("Content-Type", "application/json")
+		var body models.User
+		json.NewDecoder(r.Body).Decode(&body)
+		users = append(users, body)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
 }
 
@@ -41,14 +83,17 @@ func handleUser(w http.ResponseWriter, r *http.Request) {
 //needed to provide our web service
 type data struct {
 	PageTitle string
+	Data      []models.User
 }
 
 //In main we will handle all requests to the server
 func main() {
+	users = append(users, *d)
 	r := mux.NewRouter()
 	fs := http.FileServer(http.Dir("assets/"))
 	http.Handle("/assets/", http.StripPrefix("/static/", fs))
 	r.HandleFunc("/", handleRoot)
-	r.HandleFunc("/users/{id}", handleUser)
+	r.HandleFunc("/users", handleUser).Methods("GET", "POST")
+	r.HandleFunc("/users/{id}", handleUser).Methods("GET")
 	http.ListenAndServe(":8080", r)
 }
