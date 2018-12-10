239b2e8d18ac5d643787c4c852ddbc06298f71c9
Gladwin Tshepo Mohlamonyane <g.mohlamonyane@gmail.com>
2018-12-11 01:39:21 +0200

    making the the landing page look pretty

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,10 +2,24 @@ window.onload = function() {
   let login = document.getElementById("login");
   let register = document.getElementById("register");
   let close = document.querySelector(".modal .delete");
-  let  modal = document.querySelectorAll(".modal");
+  let modal = document.querySelectorAll(".modal");
+  let log_username = document.getElementById("log_username");
+  
+  log_username.onchange = function() {
+    //regular expression that will match most emails and tell the user if that account is valid
+    var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var found = log_username.value.match(regex);
+    
+    if (log_username.classList.contains("is-success") == true && found == null)
+      log_username.classList.remove("is-success");
+    else if (log_username.classList.contains("is-success") == false && found != null)
+      log_username.classList.add("is-success");
+  }
+  
   login.onclick = function(){
     modal[0].classList.add("is-active");
   }
+  
   close.onclick = function(){
     modal[0].classList.remove("is-active");
   }
