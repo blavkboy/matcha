b8268e4a185178ecfed6832c9b3e53f50938034c
Gladwin MOHLAMONYANE <gmohlamo@e5r11p3.wethinkcode.co.za>
2018-12-12 17:20:39 +0200

    made my login and register modals look nice, now I have to connect them

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,26 +1,35 @@
 window.onload = function() {
   let login = document.getElementById("login");
   let register = document.getElementById("register");
-  let close = document.querySelector(".modal .delete");
+  let close_login = document.querySelector("#login_modal .delete");
+  let close_register = document.querySelector("#register_modal .delete");
   let modal = document.querySelectorAll(".modal");
-  let log_username = document.getElementById("log_username");
+  let reg_email = document.getElementById("reg_email");
   
-  log_username.onchange = function() {
+  reg_email.onchange = function() {
     //regular expression that will match most emails and tell the user if that account is valid
     var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    var found = log_username.value.match(regex);
+    var found = reg_email.value.match(regex);
     
-    if (log_username.classList.contains("is-success") == true && found == null)
-      log_username.classList.remove("is-success");
-    else if (log_username.classList.contains("is-success") == false && found != null)
-      log_username.classList.add("is-success");
+    if (reg_email.classList.contains("is-success") == true && found == null)
+      reg_email.classList.remove("is-success");
+    else if (reg_email.classList.contains("is-success") == false && found != null)
+      reg_email.classList.add("is-success");
   }
   
   login.onclick = function(){
     modal[0].classList.add("is-active");
   }
+
+  register.onclick = function(){
+    modal[1].classList.add("is-active");
+  }
   
-  close.onclick = function(){
+  close_login.onclick = function(){
     modal[0].classList.remove("is-active");
+  }
+
+  close_register.onclick = function(){
+    modal[1].classList.remove("is-active");
   }
 }
